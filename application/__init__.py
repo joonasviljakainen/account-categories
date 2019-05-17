101dee4098e4939b3d42840d3dd2b9fa38142982
joonas <jonsterivilja@hotmail.com>
2019-05-17 19:46:59 +0300

    1. osan tehtävät tehty, tietokantaa aloitettu

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+# SQLALCHEMY
+from flask_sqlalchemy import SQLAlchemy
+
+# set up transaction db
+app.conficg["SQLALCHEMY_DATABASE_URI] = "sqlite:///tseks.db"
+
+# request constant logging & debug
+app.config["SQLALCHEMY_ECHO"] = True
+
+# Create database object
+db = SQLAlchemy(app)
+
 from application import views
+
+# Creating all database tables
+
+db.create_all()
