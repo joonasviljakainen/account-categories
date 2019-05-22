48d55788cc2641cd4d5b10a16720d75a6e328ad3
joonas <jonsterivilja@hotmail.com>
2019-05-22 20:46:10 +0300

    basic login & logout implemented

--- application/__init__.py
+++ application/__init__.py
@@ -18,5 +18,21 @@ from application.transactions import models
 from application.auth import models
 from application.auth import views
 
+# Login
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "You need to log in to do that."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 # Creating all database tables
 db.create_all()
