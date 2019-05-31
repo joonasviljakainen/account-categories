6ddaccbe8d387a1a1b39ff153a875cd9623b080c
joonas <jonsterivilja@hotmail.com>
2019-05-31 11:38:09 +0300

    Can update account name

--- application/bankaccounts/views.py
+++ application/bankaccounts/views.py
@@ -16,6 +16,34 @@ def get_bankaccount(bankaccount_id):
         return render_template("/bankaccounts/singlebankaccount.html", account = bankaccount, transactions=bankaccount.transactions)
 
 
+@app.route("/bankaccounts/<bankaccount_id>/deletion", methods = ["POST"])
+@login_required
+def delete_bankaccount(bankaccount_id):
+    u = current_user
+    acc = BankAccount.query.filter_by(id=bankaccount_id).first()
+
+    if u.id == acc.user_id:
+        print("FOUND, DELETING ACCOUNT " + str(acc.id))
+
+        db.session.delete(acc)
+        db.session.commit()
+
+        return redirect(url_for("bankaccounts"))
+
+    return "not found"
+
+@app.route("/bankaccounts/<bankaccount_id>/names", methods=["POST"])
+def update_bankaccount(bankaccount_id):
+    u = current_user
+    a = BankAccount.query.get(bankaccount_id)
+
+    if a.user_id == u.id:
+        a.name = request.form.get("accountname")
+        db.session().commit()
+        return redirect(url_for("get_bankaccount", bankaccount_id=bankaccount_id))
+    
+    return "ERROR"
+
 @app.route("/bankaccounts", methods = ["GET", "POST"])
 @login_required
 def bankaccounts():
