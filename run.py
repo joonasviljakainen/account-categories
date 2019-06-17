a1528e457b545cd59aacec107ea6bca0321a82a4
joonas <jonsterivilja@hotmail.com>
2019-06-17 19:48:58 +0300

    Added variable for heroku production deployment

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 from application import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    if (os.environ.get("HEROKU") == 1):
+        print("RUNNING IN PRODUCTION MODE")
+        app.run(host="0.0.0.0")    
+    else:
+        print("RUNNING IN DEBUG MODE")
+        app.run(debug=True)
     #app.run(host="0.0.0.0")
